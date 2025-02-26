ec0c80a09f754f840083dc30b47298e588c903b7
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-26 13:27:16 -0500

    Creates an array and prints its content

--- src/Grid.java
+++ src/Grid.java
@@ -1,5 +1,27 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of the class:
+ */
+
 public class Grid{
     public static void main(String args[]){
+        Integer[][] grid;
+        grid = new Integer[3][5];
 
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<5;j++) {
+               grid[i][j]=i+j;
+            }
+        }
+
+        for(int i=0;i<3;i++) {
+            System.out.print("| ");
+            for(int j=0;j<5;j++) {
+               System.out.print(grid[i][j] + " ");
+            }
+            System.out.print("|");
+            System.out.println();
+        }
     }
 }
