99d788be1d832b3b992e4a1a8c9177782174bf88
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-03 16:01:06 -0500

    Added toString()

--- src/Landscape.java
+++ src/Landscape.java
@@ -103,6 +103,12 @@ public class Landscape {
      * Returns a String representation of the Landscape.
      */
     public String toString() {
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(landscape[j][i]);
+            }
+            System.out.print("\n");
+        }
     }
 
     /**
