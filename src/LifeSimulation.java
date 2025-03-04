9d310420f59bfffa3442f3430c051a407b0ba729
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-03-03 22:46:43 -0500

    Completed LifeSimulation Class

--- src/LifeSimulation.java
+++ src/LifeSimulation.java
@@ -8,11 +8,11 @@
  public class LifeSimulation {
 
     public static void main(String[] args) throws InterruptedException {
-        Landscape scape = new Landscape(5, 4, .25);
+        Landscape scape = new Landscape(100, 100, .25);
 
         LandscapeDisplay display = new LandscapeDisplay(scape, 6);
 
-        for (int i = 0; i<5; i++){
+        for (int i = 0; i<100; i++){
             scape.advance();
             display.repaint();
             Thread.sleep(250);
